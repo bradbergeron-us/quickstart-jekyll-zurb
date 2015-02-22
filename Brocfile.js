6adfb132cef47a95dee25c7ebfe615d8d87c658e
Lorenzo Villani <lorenzo@villani.me>
2015-02-22 13:24:31 +0100

    Automatically rebuild when a site file changes

--- Brocfile.js
+++ Brocfile.js
@@ -2,15 +2,37 @@ var broccoli = require('broccoli');
 var compileSass = require('broccoli-sass');
 var execSync = require('exec-sync');
 var injectLivereload = require('broccoli-inject-livereload');
+var mapSeries = require('promise-map-series');
 var mergeTrees = require('broccoli-merge-trees');
+var Writer = require('broccoli-writer');
 
-function jekyll(tree) {
-    execSync('bundle exec jekyll build');
+//
+// Jekyll Compiler
+//
 
-    return injectLivereload('_jekyll');
+Jekyll.prototype = Object.create(Writer.prototype);
+Jekyll.prototype.constructor = Jekyll;
+
+function Jekyll(trees) {
+    if (!(this instanceof Jekyll)) return new Jekyll(trees);
+
+    this.inputTrees = trees;
+    this.description = 'Jekyll';
 }
 
-var scss = compileSass(['asset', '_vendor/foundation/scss'], 'app.scss', 'asset/app.css');
-var site = jekyll('.');
+Jekyll.prototype.write = function(readTree, destDir) {
+    return mapSeries(this.inputTrees, function iterator(tree) {
+        return readTree(tree);
+    }).then(function onceDone() {
+        execSync('bundle exec jekyll build -d ' + destDir);
+    });
+};
 
-module.exports = mergeTrees([site, scss]);
+//
+// Build
+//
+
+var scss = compileSass(['asset', '_vendor/foundation/scss'], 'app.scss', 'asset/app.css');
+var site = Jekyll(['_layouts', '_posts', 'atom.xml', 'index.html']);
+
+module.exports = injectLivereload(mergeTrees([site, scss]));
